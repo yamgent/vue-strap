e29abd7d9ba90b44119c72325ca86b45b7e1a9d0
Jason Jiang <gisonrg@gmail.com>
2017-04-10 23:03:16 +0800

    Fix closeable issue to support nested closeable element.

--- src/directives/Closeable.js
+++ src/directives/Closeable.js
@@ -7,10 +7,12 @@ module.exports = {
     let message = this.el.getAttribute('alt') || 'Expand Content';
     this.wrapper = jQuery(this.el).parent();
     this.wrapper.attr('class', `${this.el.className} closeable-wrapper`);
-    this.wrapper.append('<span class="closeable-button label label-default" style="display: none; position: absolute; top: 0; left: 0; cursor: pointer;background: #d9534f;"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></span>');
-    this.wrapper.append(`<a class="closeable-show hidden-print" style="display: none; cursor: pointer;text-decoration: underline">${message}</a>`);
-    this.closeButton = this.wrapper.find('.closeable-button');
-    this.showButton = this.wrapper.find('.closeable-show');
+    let $closeButton = jQuery('<span class="closeable-button label label-default" style="display: none; position: absolute; top: 0; left: 0; cursor: pointer;background: #d9534f;"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></span>');
+    this.wrapper.append($closeButton);
+    let $showLabel = jQuery(`<a class="closeable-show hidden-print" style="display: none; cursor: pointer;text-decoration: underline">${message}</a>`);
+    this.wrapper.append($showLabel);
+    this.closeButton = $closeButton;
+    this.showButton = $showLabel;
     this.closeButton.click(this.onClose.bind(this));
     this.showButton.click(this.onShow.bind(this));
     this.wrapper.on('mouseover', this.onMouseOver.bind(this));
