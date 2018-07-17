99d077395427e87e320917fe521fe27f9502ffdb
Tan Wang Leng <tanwangleng@outlook.com>
2018-07-17 14:47:59 +0800

    Merge webpack.config.js to webpack.build.js
    webpack.config.js contains the configuration for documentation building.
    webpack.build.js inherits these configuration, but tweaks some options
    to specialize it for library building.
    
    We no longer have the functionality for building documentation.
    
    Let's remove the configuration for documentation building, while still
    retaining the library building capability, by merging webpack.config.js
    to webpack.build.js.

--- webpack.build.js
+++ webpack.build.js
@@ -1,14 +1,67 @@
-var config = require('./webpack.config.js')
+var webpack = require('webpack')
+var path = require('path')
 
-config.entry = {
-  'vue-strap': './src/index.js',
+module.exports = {
+  entry: {
+    'vue-strap': './src/index.js',
+  },
+  output: {
+    filename: './dist/[name].js',
+    library: 'VueStrap',
+    libraryTarget: 'umd'
+  },
+  node: {
+    fs: "empty"
+  },
+  resolve: {
+    root: path.resolve('./')
+  },
+  module: {
+    loaders: [
+      {test: /\.vue$/, loader: 'vue' },
+      {
+        test: /\.js$/,
+        exclude: /node_modules|vue\/src|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
+        loader: 'babel'
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader?root=./docs/" },
+      {test: /\.scss$/, loader: "style!css!sass"},
+      {test: /\.less$/, loader: "style-loader!css-loader!less-loader"},
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      { test: require.resolve("jquery"), loader: "expose-loader?jQuery" }
+    ]
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.ProvidePlugin({
+      jQuery: 'jquery'
+    })
+  ]
+};
+
+
+if (process.env.NODE_ENV === 'production') {
+  delete module.exports.devtool;
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.ProvidePlugin({
+      jQuery: 'jquery'
+    })
+  ];
 }
-
-config.output = {
-  filename: './dist/[name].js',
-  library: 'VueStrap',
-  libraryTarget: 'umd'
-}
-
-
-module.exports = config
