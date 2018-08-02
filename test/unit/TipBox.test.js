6417bcf71b7284e0fc083da35bad8c28f4fb5acb
Tan Wang Leng <tanwangleng@outlook.com>
2018-08-02 12:18:19 +0800

    Add jest testing

---
+++ test/unit/TipBox.test.js
@@ -0,0 +1,14 @@
+import TipBox from '../../src/TipBox.vue'
+
+describe('TipBox.vue', () => {
+  it('renders li for each item in props.items', () => {
+    /*
+    const items = ['', '']
+    const wrapper = shallow(List, {
+      propsData: { items }
+    })
+    expect(wrapper.findAll('li')).toHaveLength(items.length)
+    */
+    expect(true).toBe(false);
+  })
+})
